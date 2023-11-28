cb9464ce874ef23b21e54f69c6de44aeef5361e4
Anthony Lispet <anthony.lispet@zorgi.be>
2023-11-28 16:43:53 +0100

    correction route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'football-results',loadChildren: () => import('./football-results/football-results.module').then(m => m.FootballResultsModule)},
+  { path: '',loadChildren: () => import('./football-results/football-results.module').then(m => m.FootballResultsModule)},
   { path: '**', redirectTo: '/football-results' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash : true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
